c638e1f8575b62daabb5740d8a67f05c172fe5eb
mohamedFawzei <mohamedfawzeix@gmail.com>
2025-12-23 15:40:51 +0200

    Create Vite app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <main>
+      <h2 className="font-robert-regular text-yellow-300">Weclome to Awwwards</h2>
+    </main>
+  );
+}
